337d820c47dd8a8003768350616c4c8866f973b5
Hernán Marrapodi <69157896+marrahenzo@users.noreply.github.com>
2022-10-21 15:05:27 -0300

    Give the add buttons functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,69 @@
-function App() {
-  return <div className='App'></div>;
+import Education from './Components/Education';
+import Experience from './Components/Experience';
+import Input from './Components/Input';
+import React, { Component } from 'react';
+import uniqid from 'uniqid';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      educationElements: [<Education key={uniqid()} />],
+      experienceElements: [<Experience key={uniqid()} />]
+    };
+  }
+
+  addEducationElement = () => {
+    this.setState((prevState) => {
+      return {
+        educationElements: [
+          ...prevState.educationElements,
+          <Education key={uniqid()} />
+        ]
+      };
+    });
+  };
+
+  addExperienceElement = () => {
+    this.setState((prevState) => {
+      return {
+        experienceElements: [
+          ...prevState.experienceElements,
+          <Experience key={uniqid()} />
+        ]
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <form onSubmit={(event) => event.preventDefault()}>
+          <div id='personal-data'>
+            <Input
+              label='Full Name'
+              type='text'
+              placeholder='Hernán Marrapodi'
+            />
+          </div>
+          <div id='education'>
+            {this.state.educationElements}
+            <button type='button' onClick={this.addEducationElement}>
+              +
+            </button>
+          </div>
+          <div id='experience'>
+            {this.state.experienceElements}
+            <button type='button' onClick={this.addExperienceElement}>
+              +
+            </button>
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
