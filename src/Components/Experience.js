f5c765a3b622441d8b87b2c8b67393d287877ffa
Hernán Marrapodi <69157896+marrahenzo@users.noreply.github.com>
2022-10-21 19:11:55 -0300

    Add class names and import css

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -5,15 +5,36 @@ class Experience extends Component {
   render() {
     return (
       <div className='experience'>
-        <Input label='Company name' type='text' placeholder='MarraSoft Inc.' />
+        <Input
+          label='Company name'
+          type='text'
+          placeholder='MarraSoft Inc.'
+          className='job-name'
+        />
         <Input
           label='Position'
           type='text'
           placeholder='Full-Stack Developer'
+          className='job-position'
         />
-        <Input label='Main Task(s)' type='text' placeholder='Web development' />
-        <Input label='Worked from' type='date' placeholder='' />
-        <Input label='Worked until' type='date' placeholder='' />
+        <Input
+          label='Main Task(s)'
+          type='text'
+          placeholder='Web development'
+          className='job-tasks'
+        />
+        <Input
+          label='Worked from'
+          type='date'
+          placeholder=''
+          className='job-from'
+        />
+        <Input
+          label='Worked until'
+          type='date'
+          placeholder=''
+          className='job-until'
+        />
         <button onClick={() => this.props.delete(this.props.listId)}>X</button>
       </div>
     );
