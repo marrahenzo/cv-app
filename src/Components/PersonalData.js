207c7aeb5190ba0b2f000d7291d16d4a47279e10
Hernán Marrapodi <69157896+marrahenzo@users.noreply.github.com>
2022-10-24 15:55:57 -0300

    Import styles and add ids to input components

--- src/Components/PersonalData.js
+++ src/Components/PersonalData.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import Image from './Image';
 import Input from './Input';
+import '../Styles/PersonalData.css';
 
 class PersonalData extends Component {
   render() {
     return (
       <div id='personal-data'>
-        <Image />
-        <Input label='Full Name' type='text' placeholder='Hernán Marrapodi' />
-        <Input label='Phone Number' type='number' placeholder='1234-5678' />
+        <Image id='picture' />
+        <Input
+          label='Full Name'
+          type='text'
+          placeholder='Hernán Marrapodi'
+          id='name'
+        />
+        <Input
+          label='Phone Number'
+          type='number'
+          placeholder='1234-5678'
+          id='phone'
+        />
         <Input
           label='E-mail'
           type='email'
           placeholder='info@totallylegitemail.com'
+          id='email'
         />
       </div>
     );
