d97fd22bf2ca88fcdd40e061687b3c36a5921ba7
Hernán Marrapodi <69157896+marrahenzo@users.noreply.github.com>
2022-10-25 10:03:45 -0300

    Add comment

--- src/App.js
+++ src/App.js
@@ -94,6 +94,7 @@ class App extends Component {
             {this.state.experienceElements}
             <AddButton onclick={this.addExperienceElement} />
           </div>
+          {/*Remove later if unneeded*/}
           <button id='button-submit'>Submit</button>
         </form>
       </div>
